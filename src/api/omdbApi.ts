26d83ef8f74502615c89845ab039b71169c0e7c4
Akshaybagai52 <akshaybagai52@gmail.com>
2024-11-23 11:42:17 +0530

    chore: added env for vercel

--- src/api/omdbApi.ts
+++ src/api/omdbApi.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const API_BASE_URL = 'https://www.omdbapi.com/';
 const API_KEY = import.meta.env.VITE_MOVIE_KEY;
 
+console.log(import.meta.env.VITE_MOVIE_KEY, "import.meta.env.VITE_MOVIE_KEY")
 export const fetchMovies = async (searchTerm:string, page:number) => {
   const response = await axios.get(API_BASE_URL, {
     params: {
